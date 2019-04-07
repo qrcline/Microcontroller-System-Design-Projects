f5ae92a477e536f6069b7fd94ac49b879793aec0
Quinton Cline <clineq@spu.edu>
2019-04-07 13:57:28 -0700

    Star Blaster-Mostly done
    TODO- Twinkle starts

--- HW-2- SpaceINveader/Clineq_Quinton_HW2.cydsn/main.c
+++ HW-2- SpaceINveader/Clineq_Quinton_HW2.cydsn/main.c
@@ -11,6 +11,7 @@
 */
 #include "project.h"
 #include "stdlib.h"
+#include  "stdbool.h"
 
 void drawStars(int16 starArray[])
 {
@@ -32,21 +33,27 @@ void  drawScope(int16 x,int16 y,int32 color)
 //Draws the blast 
 //X and Y is the center of the circle
 //Size for normal is 0 and bigger is a greater specified number
-void drawBlast(int16 x,int16 y,int16 size,int16 starArray[])
+void drawBlast(int16 x,int16 y,int16 size,int32 color,int16 starArray[])
 {
    
     int16 i=0;
-    for(i=0;i<(25+size);i++)
+    for(i=0;i<(40+size);i++)
     {
-     GLCD_DrawCircle(x,y,i,GLCD_BLUE);
+     GLCD_DrawCircle(x,y,i,color);
     }
-     for(i=0;i<(25+size);i++)
+     for(i=0;i<(40+size);i++)
     {
      GLCD_DrawCircle(x,y,i,GLCD_BLACK);
     }
-    
+       drawStars(starArray);
     drawScope(x,y,GLCD_YELLOW);
-    drawStars(starArray);
+ 
+}
+void ledSet(int16 r,int16 g, int16 b)
+{
+    R_LED_Write(r);
+    G_LED_Write(g);
+    B_LED_Write(b);
 }
 
 
@@ -56,6 +63,11 @@ int main(void)
     int16 i=0;
     int16 x=62;
     int16 y=62;
+    uint32 counter=0;
+    bool blasterReady=true;
+    bool hyperblasterReady=true;
+    
+    
     for(i=0;i<300;i++)
     {
         stars[i]=rand()%132;
@@ -68,18 +80,28 @@ int main(void)
     GLCD_Clear(GLCD_BLACK);
     Backlight_Write(1);
     drawStars(stars);
-    drawScope(62,62,GLCD_YELLOW);
-    //drawBlast(62,62,0,stars);
+    drawScope(x,y,GLCD_YELLOW);
+    ledSet(1,1,0); //Set to blue
 
-    int16 counter=0;
+    
+
+
     while(1)
     {
 //        if (counter % 100000 ==0)
 //        {
 //            drawBlast(62,62,0,stars);
 //        }
-        
-        
+        if(counter==500000)
+        {
+            blasterReady=true;
+            ledSet(1,0,1);//Set to green
+        }
+         if(counter==1000000)
+        {
+            hyperblasterReady=true;
+            ledSet(1,1,0); //Set to blue
+        }
         // If A pressed, move up
      while (A_Button_Read()==0)
     {
@@ -121,8 +143,23 @@ int main(void)
     
     // If joystick center pushed, turn on Blue and Red LEDs and draw lines in White
     if (Joystick_button_Read()==0) 
-    {
-           drawBlast(x,y,0,stars);  
+    {      if(hyperblasterReady==true)
+            {
+                drawBlast(x,y,0,GLCD_BLUE,stars);  
+                blasterReady=false;
+                hyperblasterReady=false;
+                counter=0;
+                ledSet(0,1,1); //Set to red
+            }
+        
+            else if(blasterReady==true)
+            {
+                drawBlast(x,y,0,GLCD_GREEN,stars);  
+                blasterReady=false;
+                counter=0;
+                ledSet(0,1,1); //Set to red
+            }
+        
     }   
             
             
